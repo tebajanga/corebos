84a74942b82340c4da95bf9594cdbfc4237f4e1d
Timothy Anthony <anthony.timothy90@gmail.com>
2018-08-28 02:59:47 +0300

    feat(Abonado) Handling Abonado class

--- modules/Abonado/Abonado.php
+++ modules/Abonado/Abonado.php
@@ -10,12 +10,12 @@
 require_once 'data/CRMEntity.php';
 require_once 'data/Tracker.php';
 
-class ModuleClass extends CRMEntity {
+class Abonado extends CRMEntity {
 	public $db;
 	public $log;
 
-	public $table_name = 'vtiger_MODULE_NAME_LOWERCASE';
-	public $table_index= 'MODULE_NAME_LOWERCASEid';
+	public $table_name = 'vtiger_abonado';
+	public $table_index= 'abonadoid';
 	public $column_fields = array();
 
 	/** Indicator if this is a custom module or standard module */
@@ -24,7 +24,7 @@ class ModuleClass extends CRMEntity {
 	/**
 	 * Mandatory table for supporting custom fields.
 	 */
-	public $customFieldTable = array('vtiger_MODULE_NAME_LOWERCASEcf', 'MODULE_NAME_LOWERCASEid');
+	public $customFieldTable = array('vtiger_abonadocf', 'abonadoid');
 	// related_tables variable should define the association (relation) between dependent tables
 	// FORMAT: related_tablename => array(related_tablename_column[, base_tablename, base_tablename_column[, related_module]] )
 	// Here base_tablename_column should establish relation with related_tablename_column
@@ -35,15 +35,15 @@ class ModuleClass extends CRMEntity {
 	/**
 	 * Mandatory for Saving, Include tables related to this module.
 	 */
-	public $tab_name = array('vtiger_crmentity', 'vtiger_MODULE_NAME_LOWERCASE', 'vtiger_MODULE_NAME_LOWERCASEcf');
+	public $tab_name = array('vtiger_crmentity', 'vtiger_abonado', 'vtiger_abonadocf');
 
 	/**
 	 * Mandatory for Saving, Include tablename and tablekey columnname here.
 	 */
 	public $tab_name_index = array(
 		'vtiger_crmentity' => 'crmid',
-		'vtiger_MODULE_NAME_LOWERCASE'   => 'MODULE_NAME_LOWERCASEid',
-		'vtiger_MODULE_NAME_LOWERCASEcf' => 'MODULE_NAME_LOWERCASEid',
+		'vtiger_abonado'   => 'abonadoid',
+		'vtiger_abonadocf' => 'abonadoid',
 	);
 
 	/**
@@ -52,52 +52,66 @@ class ModuleClass extends CRMEntity {
 	public $list_fields = array(
 		/* Format: Field Label => array(tablename => columnname) */
 		// tablename should not have prefix 'vtiger_'
-		'MODULE_NAME_LABEL'=> array('MODULE_NAME_LOWERCASE' => 'MODULE_REFERENCE_FIELD'),
+		'Abonado No'=> array('abonado' => 'abonado_no'),
+		'Numabonado'=> array('abonado' => 'numabonado'),
+		'Account Id'=> array('abonado' => 'accid'),
+		'Active'=> array('abonado' => 'active'),
 		'Assigned To' => array('crmentity' => 'smownerid')
 	);
 	public $list_fields_name = array(
 		/* Format: Field Label => fieldname */
-		'MODULE_NAME_LABEL'=> 'MODULE_REFERENCE_FIELD',
-		'Assigned To' => 'assigned_user_id'
+		'Abonado No'=> 'abonado_no',
+		'Numabonado'=> 'numabonado',
+		'Account Id'=> 'accid',
+		'Active'=> 'active',
+		'Assigned To' => 'smownerid'
 	);
 
 	// Make the field link to detail view from list view (Fieldname)
-	public $list_link_field = 'MODULE_REFERENCE_FIELD';
+	public $list_link_field = 'numabonado';
 
 	// For Popup listview and UI type support
 	public $search_fields = array(
 		/* Format: Field Label => array(tablename => columnname) */
 		// tablename should not have prefix 'vtiger_'
-		'MODULE_NAME_LABEL'=> array('MODULE_NAME_LOWERCASE' => 'MODULE_REFERENCE_FIELD')
+		'Abonado No'=> array('abonado' => 'abonado_no'),
+		'Numabonado'=> array('abonado' => 'numabonado'),
+		'Account Id'=> array('abonado' => 'accid'),
+		'Active'=> array('abonado' => 'active'),
+		'Assigned To'=> array('crmentity' => 'smownerid'),
 	);
 	public $search_fields_name = array(
 		/* Format: Field Label => fieldname */
-		'MODULE_NAME_LABEL Name'=> 'MODULE_REFERENCE_FIELD'
+		'Abonado No'=> 'abonado_no',
+		'Numabonado'=> 'numabonado',
+		'Account Id'=> 'accid',
+		'Active'=> 'active',
+		'Assigned To' => 'smownerid'
 	);
 
 	// For Popup window record selection
-	public $popup_fields = array('MODULE_REFERENCE_FIELD');
+	public $popup_fields = array('numabonado', 'accid', 'active', 'smownerid');
 
 	// Placeholder for sort fields - All the fields will be initialized for Sorting through initSortFields
 	public $sortby_fields = array();
 
 	// For Alphabetical search
-	public $def_basicsearch_col = 'MODULE_REFERENCE_FIELD';
+	public $def_basicsearch_col = 'numabonado';
 
 	// Column value to use on detail view record text display
-	public $def_detailview_recname = 'MODULE_REFERENCE_FIELD';
+	public $def_detailview_recname = 'numabonado';
 
 	// Required Information for enabling Import feature
-	public $required_fields = array('MODULE_REFERENCE_FIELD'=>1);
+	public $required_fields = array('numabonado'=>1);
 
 	// Callback function list during Importing
 	public $special_functions = array('set_import_assigned_user');
 
-	public $default_order_by = 'MODULE_REFERENCE_FIELD';
+	public $default_order_by = 'numabonado';
 	public $default_sort_order='ASC';
 	// Used when enabling/disabling the mandatory fields for the module.
 	// Refers to vtiger_field.fieldname values.
-	public $mandatory_fields = array('createdtime', 'modifiedtime', 'MODULE_REFERENCE_FIELD');
+	public $mandatory_fields = array('numabonado', 'accid', 'active');
 
 	public function save_module($module) {
 		if ($this->HasDirectImageField) {
@@ -111,9 +125,24 @@ class ModuleClass extends CRMEntity {
 	 * @param String Event Type (module.postinstall, module.disabled, module.enabled, module.preuninstall)
 	 */
 	public function vtlib_handler($modulename, $event_type) {
+		require_once 'include/utils/utils.php';
+		global $adb;
+
 		if ($event_type == 'module.postinstall') {
-			// TODO Handle post installation actions
 			$this->setModuleSeqNumber('configure', $modulename, $modulename.'-', '0000001');
+			//Add Abonado Module to Customer Portal
+			global $adb;
+			$this->setModuleSeqNumber('configure', $moduleName, 'abn-', '0000001');
+			$this->addModuleToCustomerPortal();
+
+			include_once 'vtlib/Vtiger/Module.php';
+
+			// Mark the module as Standard module
+			$adb->pquery('UPDATE vtiger_tab SET customized=0 WHERE name=?', array($moduleName));
+
+			//adds sharing accsess
+			$AbonadoModule  = Vtiger_Module::getInstance('Abonado');
+			Vtiger_Access::setDefaultSharing($AbonadoModule);
 		} elseif ($event_type == 'module.disabled') {
 			// TODO Handle actions when this module is disabled.
 		} elseif ($event_type == 'module.enabled') {
